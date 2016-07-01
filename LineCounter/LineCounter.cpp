31e18f35d7722968f1006031559acf267462fd00
Ryan Hodin <ryan951357@gmail.com>
2016-06-30 21:03:03 -0700

    Write linesInDirectoryRecursive()
    Which is like linesInDirectory(), but it calls itself again when it finds a subdirectory, so it can be used on entire trees.

--- LineCounter/LineCounter.cpp
+++ LineCounter/LineCounter.cpp
@@ -8,6 +8,7 @@ using namespace std;
 
 int linesInFile(WIN32_FIND_DATA);
 int linesInDirectory(const char*);
+int linesInDirectoryRecursive(const char*);
 
 int main(int argc, char* argv[])
 {
@@ -58,6 +59,56 @@ int linesInDirectory(string dir)
 
 	cout << "\n\nTotal number of lines in files in the directory " << _getcwd(NULL, NULL) << ": " << sum << ".\n";
 	return 0;
+}
+
+int linesInDirectoryRecursive(string dir)
+{
+	WIN32_FIND_DATA	ffd;
+
+	cout << "Target directory is " << dir << endl;
+
+	_chdir(dir.c_str());
+	dir += "\\*";
+
+	HANDLE find = INVALID_HANDLE_VALUE;
+	find = FindFirstFile(dir.c_str(), &ffd);
+
+	if (find == INVALID_HANDLE_VALUE)
+	{
+		cerr << "No files found!\n";
+		return -1;
+	}
+
+	uintmax_t sum(0);
+	size_t len(0);
+
+	do
+	{
+		if (ffd.dwFileAttributes&FILE_ATTRIBUTE_DIRECTORY)
+		{
+			cout << "\n\nFound directory " << ffd.cFileName << ".\n";
+			cout << "Counting lines in directory...\n\n";
+			len = linesInDirectoryRecursive(dir);
+			if (len != -1)
+				sum += len;
+			cout << "Done with subdirectory " << ffd.cFileName << ".\n";
+			cout << "\tRunning total of lines in " << dir << ": " << sum << ".\n\n";
+		}
+		else
+		{
+			len = linesInFile(ffd);
+			sum += len;
+			cout << "\tFile " << ffd.cFileName << ": " << len << " lines.\n";
+			cout << "\t\tRunning total: " << sum << endl << endl;
+		}
+	} while (FindNextFile(find, &ffd));
+
+	char msg[9001];
+	FormatMessage(FORMAT_MESSAGE_FROM_SYSTEM | 80, NULL, GetLastError(), 0, msg, 9001, NULL);
+	cout << "\n\n" << msg;
+
+	cout << "\n\nTotal number of lines in files in the directory " << _getcwd(NULL, NULL) << ": " << sum << ".\n";
+	return sum;
 }
 
 int linesInFile(WIN32_FIND_DATA ffd)
